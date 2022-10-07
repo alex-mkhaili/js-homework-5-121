eb1bca5a9bdea0df875abd24490b9ce4a3077616
alex-mkhaili <109855533+alex-mkhaili@users.noreply.github.com>
2022-10-07 12:54:45 +0400

    updated

--- script.js
+++ script.js
@@ -21,3 +21,106 @@
 // array.pop("pear");
 // console.log(array.length);
 // console.log(array);
+
+
+
+// #3
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// let newArray = array.map((element) => element/3);
+// console.log(newArray);
+
+
+
+// #4
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+
+// let newArray = array.filter(function(element) {
+//     if (typeof element == "number") {
+//         console.log(element);
+//     }
+// })
+
+
+
+
+// #5
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+
+// let newArrLanguages = languages.filter(function(element) {
+//     if (element.length > 3) {
+//         console.log(element);
+//     }
+// })
+
+
+
+
+// #6
+// let item = [12, 'google', 32, null, 'yahoo', 25];
+
+// let newArrItem = item.map(function(element) {
+//     if (typeof element == "number") {
+//         console.log(Math.pow(element, 2));
+//     } else if (typeof element == "string") {
+//         console.log(element.toUpperCase());
+//     }
+//     else {
+//         console.log(element);
+//     }
+// })
+
+
+
+
+// #7
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+
+// let newArrWords = words.filter(function(element) {
+//     return element.includes("m") || element.includes("M");
+// })
+// console.log(newArrWords);
+
+
+
+// #8
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+
+// let arr1and2 = arr1.concat(arr2);
+// let arr1and2and3 = arr1and2.concat(arr3);
+// console.log(arr1and2and3);
+
+
+// #9
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.forEach(function(element) {
+    
+// })??????????????????????????????????????????????????????????????????
+
+
+
+
+// #10
+// let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
+
+// numbers.filter(function(element) {
+//     if (element > 0) {
+//         console.log(element);
+//     }
+// })
+
+
+
+// #11
+// let arr = [-1, -2, -3, 4].some((element) => element > 0);
+// console.log(arr);
+
+
+
+// #12
+// let array =[23,45,32,5,87,7,3,98];
+
+// let newArr = array.sort()??????????????????????????????????????
